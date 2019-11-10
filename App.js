71f31d823a1b7805488d7af2e5c0ba1862a60360
diegogl12 <diegogl13@hotmail.com>
2019-11-09 23:54:13 -0300

    create home screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import HomeScreen from './screens/Home';
+import HomeScreen from './screens/Home/Home';
 import Checklist from './screens/Checklist';
 
 const MainNavigator = createStackNavigator({
